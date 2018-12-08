7a4da94329520eba111fe95a2f3e9f8594d2cebb
Maxi <andreolixam@gmail.com>
2018-12-08 13:10:30 -0300

    Proce 2

--- Proc.vhd
+++ Proc.vhd
@@ -8,7 +8,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Proc is
-    Port ( clk : in  STD_LOGIC;
+    port ( clk : in  STD_LOGIC;
            rst : in  STD_LOGIC;
            input : in  STD_LOGIC_VECTOR (7 downto 0);
            output : out  STD_LOGIC_VECTOR (7 downto 0));
@@ -20,31 +20,31 @@ architecture Beh_Proc of Proc is
 -- Declaración de los componentes utilziados
 
 component regs 
-    Port ( regs_clk, regs_rst, regs_we : in  std_logic;
+    port ( regs_clk, regs_rst, regs_we : in  std_logic;
            regs_rd, regs_rs : in  std_logic_vector (3 downto 0);
            regs_din : in  std_logic_vector (7 downto 0);
            regs_dout : out  std_logic_vector (7 downto 0));
 end component;
 
 component reg_a 
-    Port ( reg_a_input : in  STD_LOGIC_VECTOR (7 downto 0);
+    port ( reg_a_input : in  STD_LOGIC_VECTOR (7 downto 0);
            reg_a_output : out  STD_LOGIC_VECTOR (7 downto 0);
            reg_a_we, reg_a_clk, reg_a_rst : in  STD_LOGIC);
 end component;
 
 component reg_out
-    Port ( reg_out_input : in  STD_LOGIC_VECTOR (7 downto 0);
+    port ( reg_out_input : in  STD_LOGIC_VECTOR (7 downto 0);
            reg_out_output : out  STD_LOGIC_VECTOR (7 downto 0);
            reg_out_we, reg_out_clk, reg_out_rst : in  STD_LOGIC);
 end component;
 
 component ir
-    Port ( ir_input : in  STD_LOGIC_VECTOR (15 downto 0);
+    port ( ir_input : in  STD_LOGIC_VECTOR (15 downto 0);
            ir_output : out  STD_LOGIC_VECTOR (15 downto 0);
            ir_we, ir_clk, ir_rst : in  STD_LOGIC);
 end component;
           
-component alu port ( alu_op: in  std_logic_vector(3 downto 0);
+component alu port ( alu_op: in  std_logic_vector(2 downto 0);
            alu_a, alu_b : in  std_logic_vector (7 downto 0);
            alu_s : out  std_logic_vector (7 downto 0);
            alu_clk, alu_rst : in  std_logic);
@@ -56,7 +56,7 @@ component rom_prog port (rom_prog_addr : in  std_logic_vector (6 downto 0);
 end component; 
 
 
-component decode port (decode_input : in  std_logic_vector (8 downto 0);
+component decode port (decode_input : in  std_logic_vector (7 downto 0);
 					decode_reg_we, decode_out_we, decode_reg_a_we : out  std_logic;
 					decode_alu_op : out  std_logic_vector (2 downto 0);
 					decode_bus_sel : out  std_logic_vector (1 downto 0));
@@ -66,93 +66,113 @@ component pc port (pc_output : out STD_LOGIC_VECTOR(6 downto 0);
         pc_clk, pc_rst : in  STD_LOGIC);
 end component; 
   
+  
+-- ======================
 
--- ================
 
--- ================
--- declaración de señales usadas 
+-- Declaración de señales :
+-- *colocadas (la mayoría) en orden de salida de los componentes
 
--- Banco de registros
-signal we: std_logic; -- senal para escribir en el banco de registro 
-signal rd, rs: std_logic_vector(3 downto 0);
-signal pre_out, mux_out: std_logic_vector(7 downto 0);
--- signal ....
+-- PC
+signal sig_pc_output: STD_LOGIC_VECTOR(6 downto 0);
 
--- ================
+-- ROM de programa
+signal sig_rom_prog_output: std_logic_vector (15 downto 0);
+
+-- IR
+signal sig_ir_output: std_logic_vector (15 downto 0);
+signal sig_ir_we: std_logic;
+
+-- Decode
+signal sig_decode_out_we, sig_decode_reg_we, sig_decode_reg_a_we: std_logic;
+signal sig_decode_alu_op: std_logic_vector (2 downto 0);
+signal sig_decode_bus_sel: std_logic_vector (1 downto 0);
+
+-- Registro A
+signal sig_reg_a_output: STD_LOGIC_VECTOR (7 downto 0);
+
+-- Registro out
+signal sig_reg_out_input, sig_reg_out_output: STD_LOGIC_VECTOR (7 downto 0);
+
+-- Mux
+signal sig_mux_out: STD_LOGIC_VECTOR (7 downto 0);
+
+
+-- ======================
+
 
 begin
 
--- ================
--- Instaciacion de componentes utilziados
+-- Instaciación de componentes utilizados:
 
 -- PC
-ePC: pc port map (pc_clk => clk,
+ePC: pc port map (
+pc_output => sig_pc_output,
+pc_clk => clk,
 pc_rst => rst);
 
--- La ROM de programa
-eROM_Prog: rom_prog port map (rom_prog_addr => pc_output,
-rom_prog_output => ir_input,
+-- ROM de programa
+eROM_Prog: rom_prog port map (rom_prog_addr => sig_pc_output,
+rom_prog_output => sig_rom_prog_output,
 rom_prog_clk => clk,
 rom_prog_rst => rst);
 
 -- IR
-eIR: ir port map (ir_input,
-ir_output,
-ir_we => we,
+eIR: ir port map (ir_input => sig_rom_prog_output,
+ir_output => sig_ir_output,
+ir_we => sig_ir_we,
 ir_clk => clk,
 ir_rst => rst);
 
 -- Banco de registros
 eregs:  regs Port map (regs_clk => clk, 
 regs_rst => rst, 
-regs_we => decode_reg_we, 
-								regs_rd => ir_output(3 downto 0), 
-								regs_rs => ir_output(7 downto 4), 
-								regs_din => pre_out, 
-								regs_dout => mux_out); 
--- La ALU
-eAlu: alu port map (alu_op => decode_alu_op, 
-					          alu_a => mux_out,
-							      alu_b =>,
-							      alu_s => pre_out,
+regs_we => sig_decode_reg_we, 
+								regs_rd => sig_ir_output(3 downto 0), 
+								regs_rs => sig_ir_output(7 downto 4), 
+								regs_din => sig_reg_out_input, 
+								regs_dout => sig_mux_out); 
+-- ALU
+eAlu: alu port map (alu_op => sig_decode_alu_op, 
+					          alu_a => sig_mux_out,
+							      alu_b => sig_reg_a_output,
+							      alu_s => sig_reg_out_input,
 							      alu_clk => clk,
 							      alu_rst => rst);
 
--- El decodificador de la instrucción
-eDecode: decode port map (decode_input => ir_output(15 downto 8),
-decode_reg_we => regs_we,
-decode_out_we => reg_out_we,
-decode_reg_a_we => reg_a_we,
-decode_alu_op => alu_op,
-decode_bus_sel);
+-- Decodificador de la instrucción
+eDecode: decode port map (decode_input => sig_ir_output(15 downto 8),
+decode_reg_we => sig_decode_reg_we,
+decode_out_we => sig_decode_out_we,
+decode_reg_a_we => sig_decode_reg_a_we,
+decode_alu_op => sig_decode_alu_op,
+decode_bus_sel => sig_decode_bus_sel);
 
 -- Registro A
-eReg_A: reg_a Port map (reg_a_input => mux_out,
-reg_a_output => alu_b,
-reg_a_we,
+eReg_A: reg_a port map (reg_a_input => sig_mux_out,
+reg_a_output => sig_reg_a_output,
+reg_a_we => sig_decode_reg_a_we,
 reg_a_clk => clk,
 reg_a_rst => rst);
 
 -- Registro Out
-eReg_Out: reg_out Port map (reg_a_input => pre_out,
+eReg_Out: reg_out port map (reg_out_input => sig_reg_out_input,
 reg_out_output => output,
-reg_out_we,
+reg_out_we => sig_decode_out_we,
 reg_out_clk => clk,
 reg_out_rst => rst);
 
+-- Descripción del mux que funciona como "bus":
+-- *controlado por bus_sel
+
+sig_mux_out <= input when sig_decode_bus_sel = "10" else
+           sig_ir_output(7 downto 0) when sig_decode_bus_sel = "01" else
+           sig_mux_out;
+           
+           
 -- ================
 
 
--- ================
--- Descripción de mux que funciona como "bus"
--- controlado por bus_sel
-mux_out <= input when decode_bus_sel = "10" else
-           ir_output when decode_bus_sel = "01" else
-           reg_dout;
--- ================
-
-
--- ================
 -- Descripción de los almacenamientos
 -- Los almacenamientos que se deben decribir aca son: 
 -- <reg_a> 8 bits
@@ -170,8 +190,5 @@ mux_out <= input when decode_bus_sel = "10" else
 		  end if;
 		  
 	end process;
--- ================
-
 
 end Beh_Proc;
-
