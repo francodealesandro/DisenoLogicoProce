af7aa979574efdf700e6fab6b66db759b964da7b
francodealesandro <francodealesandro@gmail.com>
2018-12-10 21:50:07 -0300

    ALU TB

--- ALU_tb.vhd
+++ ALU_tb.vhd
@@ -6,27 +6,27 @@ use IEEE.NUMERIC_STD.ALL;
 entity alu_tb is
 end alu_tb;
 
-architecture alu_behavior of alu_tb is 
+architecture behavior of alu_tb is 
 
 	component alu
 	port(
-		    alu_a, alu_b : in  STD_LOGIC_VECTOR (7 downto 0);
-        alu_s : out  STD_LOGIC_VECTOR (7 downto 0);
-        alu_op : in  STD_LOGIC_VECTOR (2 downto 0);
-        alu_clk : in STD_LOGIC;
-		    alu_rst : in STD_LOGIC
+		    a, b : in  STD_LOGIC_VECTOR (7 downto 0);
+        s : out  STD_LOGIC_VECTOR (7 downto 0);
+        op : in  STD_LOGIC_VECTOR (2 downto 0);
+        clk : in STD_LOGIC;
+		    rst : in STD_LOGIC
 		);
 	end component;
 
-	-- alu_señales de estímulo
-	signal alu_a :  STD_LOGIC_VECTOR (7 downto 0) := "10110100";
-	signal alu_b :  STD_LOGIC_VECTOR (7 downto 0) := "10001010";
-  signal alu_op : STD_LOGIC_VECTOR (2 downto 0) := "110";
-  signal alu_clk :  STD_LOGIC := '0';
-	signal alu_rst :  STD_LOGIC := '0';
+	-- señales de estímulo
+	signal a :  STD_LOGIC_VECTOR (7 downto 0) := "10110100";
+	signal b :  STD_LOGIC_VECTOR (7 downto 0) := "10001010";
+  signal op : STD_LOGIC_VECTOR (2 downto 0) := "110";
+  signal clk :  STD_LOGIC := '0';
+	signal rst :  STD_LOGIC := '0';
 	
-	-- alu_señales a observar
-	signal alu_s:  std_logic_vector(7 downto 0);
+	-- señales a observar
+	signal s:  std_logic_vector(7 downto 0);
 
   constant delay: time:= 10 ns;
 
@@ -34,65 +34,65 @@ begin
 
 	-- Instantiate the Unit Under Test (UUT)
 	uut: alu port map(
-		alu_a => alu_a,
-		alu_b => alu_b,
-		alu_s => alu_s,
-		alu_op => alu_op,
-		alu_clk => alu_clk,
-		alu_rst => alu_rst
+		a => a,
+		b => b,
+		s => s,
+		op => op,
+		clk => clk,
+		rst => rst
 	);
 
    Pop: process 
    begin
-      alu_op <= alu_op + 1; 
+      op <= op + 1; 
       wait for delay;
    end process;
    
-  Passert: process(alu_s)
+  Passert: process(s)
   begin
-    if alu_rst = '1' then
-      assert alu_s = "00000000" report "Error al inducir alu_rst" severity failure;
+    if rst = '1' then
+      assert s = "00000000" report "Error al inducir rst" severity failure;
     else
-      case alu_op is
+      case op is
         when "000" =>
-          assert alu_s = alu_a report "Error al asignar alu_s <= alu_a" severity failure;
+          assert s = a report "Error al asignar s <= a" severity failure;
         when "001" =>
-          assert alu_s = (alu_a(6 downto 0) & '0') report "Error al asignar alu_s <= alu_a sll 1" severity failure;
+          assert s = (a(6 downto 0) & '0') report "Error al asignar s <= a sll 1" severity failure;
         when "010" =>
-          assert alu_s = (alu_a + alu_b) report "Error al asignar alu_s <= alu_a + alu_b" severity failure;
+          assert s = (a + b) report "Error al asignar s <= a + b" severity failure;
         when "011" =>
-          assert alu_s = (alu_a - alu_b) report "Error al asignar alu_s <= alu_a - alu_b" severity failure;
+          assert s = (a - b) report "Error al asignar s <= a - b" severity failure;
         when "100" =>
-          assert alu_s = (alu_a and alu_b) report "Error al asignar alu_s <= alu_a and alu_b" severity failure;
+          assert s = (a and b) report "Error al asignar s <= a and b" severity failure;
         when "101" =>
-          assert alu_s = (alu_a or alu_b) report "Error al asignar alu_s <= alu_a or alu_b" severity failure;
+          assert s = (a or b) report "Error al asignar s <= a or b" severity failure;
         when "110" =>
-          assert alu_s = (alu_a xor alu_b) report "Error al asignar alu_s <= alu_a xor alu_b" severity failure;
+          assert s = (a xor b) report "Error al asignar s <= a xor b" severity failure;
         when "111" =>
-          assert alu_s = ('0' & alu_a(7 downto 1)) report "Error al asignar alu_s <= alu_a srl 1" severity failure;
+          assert s = ('0' & a(7 downto 1)) report "Error al asignar s <= a srl 1" severity failure;
         when others =>
-          assert alu_s = alu_a report "Error al asignar alu_s <= alu_a" severity failure;
+          assert s = a report "Error al asignar s <= a" severity failure;
         end case;
     end if;
    end process;
    
    Pclk: process 
    begin
-      alu_clk <= '0';
+      clk <= '0';
       wait for delay/2;
-      alu_clk <= '1';
+      clk <= '1';
       wait for delay/2;
    end process;       
    
    PRst: process 
    begin
-      alu_rst <= '1'; 
+      rst <= '1'; 
       wait for delay;
-      alu_rst <= '0';     
+      rst <= '0';     
       wait for delay*16;
-      alu_rst <= '1'; 
+      rst <= '1'; 
       wait for delay;
-      alu_rst <= '0';     
+      rst <= '0';     
       wait;
    end process; 
 
